1611d374d2b69fa27604f71c08437f6617ba40d6
Matthew Pelletier <matthewp227@gmail.com>
2024-06-08 21:23:26 -0400

    Added camera controlls

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField] int sens;
+    [SerializeField] int lockVertMin, lockVertMax;
+    [SerializeField] bool invertY;
+
+    float rotX;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float mouseY = Input.GetAxis("Mouse Y") * sens * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * sens * Time.deltaTime;
+
+        if (invertY)
+        {
+            rotX += mouseY;
+        }
+        else
+        {
+            rotX -= mouseX;
+        }
+        //clamp the rotX on the x axis
+
+        rotX = Mathf.Clamp(rotX, lockVertMin, lockVertMax);
+
+        //rotate the camera on the x-axis
+        transform.localRotation = Quaternion.Euler(rotX, 0, 0);
+
+        //rotate the player on the y-xis
+        transform.parent.Rotate(Vector3.up * mouseX);
     }
 }
