5aaedd271a08700015a9a34f074b15f75cab69ac
Max Tomlinson <155977994+CloverzField-Z@users.noreply.github.com>
2024-06-09 15:18:29 -0400

    Basic movement(WASD and Jumping). Sprint function created but not working, but I'm pushing to test if it's an issue with my older computer or not onto my laptop

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] CharacterController controller;
+
+    [SerializeField] float speed;
+    [SerializeField] float sprintMod;
+    [SerializeField] int numOfJumps;
+    [SerializeField] int jumpSpeed;
+    [SerializeField] int gravity;
+    [SerializeField] float staminaDrainRate;
+    [SerializeField] int stamina;
+
+    int jumpCount;
+    Vector3 moveDirection;
+    Vector3 playerVelocity;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +28,37 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        movement();
+        sprint();
+    }
+
+    void movement()
+    {
+        if(controller.isGrounded)
+        {
+            jumpCount = 0;
+            playerVelocity = Vector3.zero;
+        }
+        moveDirection = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
+        controller.Move(moveDirection * speed * Time.deltaTime);
+        if(Input.GetButtonDown("Jump") && jumpCount < numOfJumps)
+        {
+            jumpCount++;
+            playerVelocity.y = jumpSpeed;
+        }
+        playerVelocity.y -= gravity * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+    void sprint()
+    {
+        if(Input.GetButtonDown("Sprint"))
+        {
+            speed *= sprintMod;
+        }
+        else if (Input.GetButtonUp("Sprint"))
+        {
+            speed /= sprintMod;
+        }
     }
 }
