1b7e7098092035fa1da4f71ed09f0e67ed6663c5
BDJMartinez <145031898+BDJMartinez@users.noreply.github.com>
2024-06-13 12:48:12 -0400

    mixer

--- Assets/Scripts/MixerEnemyScript.cs
+++ Assets/Scripts/MixerEnemyScript.cs
@@ -33,8 +33,8 @@ public class MixerEnemyScript : EnemyAI
         if (distanceToPlayer < meleeAttackRange)
             playerInMeleeRange = true;
 
-        //if (distanceToPlayer < playerInRangedAttackRange)
-        //    playerInRangedAttackRange = true;
+        if (distanceToPlayer < rangedAttackRange)
+            playerInRangedAttackRange = true;
 
         isAttacking = true;
         if (playerInRangedAttackRange && !playerInMeleeRange)
