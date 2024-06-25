fc644c1b88a816118378844a7d5122462a523fd9
Matthew Pelletier <matthewp227@gmail.com>
2024-06-24 23:21:51 -0400

    updated range to set lower than 1

--- Assets/Scripts/WeaponStats.cs
+++ Assets/Scripts/WeaponStats.cs
@@ -15,6 +15,6 @@ public class WeaponStats : ScriptableObject
     [Range(1, 10)] public int destroyTime;
     public PlayerProjectile projectile;
     public GameObject HitEffect;
-    [Range(1, 10)] public float arc;
+    [Range(0, 10)] public float arc;
 
 }
